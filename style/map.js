bc97c47264239d591bc4e4c72aaf8c28fd5574f6
Teodora <tnikolova@hawk.iit.edu>
2019-11-12 21:54:07 -0600

    adding places to maps

--- style/map.js
+++ style/map.js
@@ -2,7 +2,15 @@
       function initMap() {
       
        var map = new google.maps.Map(
-       document.getElementById('map'), {zoom: 8, center: {lat: -34.397, lng: 150.644});
+       document.getElementById('map'), {zoom: 8, center: {lat: -34.397, lng: 150.644}
+       );
       }
       
 google.maps.event.addDomListener(window, 'load', initMap);  
+
+41.893701, -87.630018 - tao
+
+41.896005, -87.671676 - yuzu
+41.891125, -87.656239 - butterfly
+41.924254, -87.636953 - naoki sushi
+41.943351, -87.649569 - Rollapalooza
