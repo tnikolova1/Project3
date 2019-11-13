7b7cb9580ce46cd807d8544ca0eca8dc7fe00850
Teodora <tnikolova@hawk.iit.edu>
2019-11-12 22:08:44 -0600

    Update map.js

--- style/map.js
+++ style/map.js
@@ -1,16 +1,13 @@
 
       function initMap() {
       
-       var map = new google.maps.Map(
-       document.getElementById('map'), {zoom: 8, center: {lat: -34.397, lng: 150.644}
-       );
+    var map = document.getElementById('map');
+	var tao = new google.maps.LatLng(41.893701, -87.630018);
+	var yuzu = new google.maps.LatLng(41.896005, -87.671676);
+	var butterfly = new google.maps.LatLng(41.891125, -87.656239);
+	var naoki = new google.maps.LatLng(41.924254, -87.636953);
+	var rolla = new google.maps.LatLng(41.943351, -87.649569);
       }
       
 google.maps.event.addDomListener(window, 'load', initMap);  
 
-41.893701, -87.630018 - tao
-
-41.896005, -87.671676 - yuzu
-41.891125, -87.656239 - butterfly
-41.924254, -87.636953 - naoki sushi
-41.943351, -87.649569 - Rollapalooza
