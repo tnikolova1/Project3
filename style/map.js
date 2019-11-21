a84db52bf09a08cc26559236c5c3a363b12016f1
Teodora <tnikolova@hawk.iit.edu>
2019-11-21 13:16:41 -0600

    Update map.js

--- style/map.js
+++ style/map.js
@@ -1,6 +1,14 @@
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
 
-
- function initMap() {
+ /*function initMap() {
 
     var map = document.getElementById('map');
 	var tao = new google.maps.LatLng(41.893701, -87.630018);
@@ -45,4 +53,4 @@
 }
 
 //google.maps.event.addDomListener(window, 'load', initMap);
-window.addEventListener('load', initMap );
+window.addEventListener('load', initMap ); */
